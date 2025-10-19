1b99952c081925f606d30147587a5ecfa7551f64
Bogumila Okrojek <01180727@pw.edu.pl>
2025-10-19 14:06:19 +0200

    Added more tests for fcmkiimputer; improved FCMKImputer performance; additonal corrections

--- ficaria/missing_imputation.py
+++ ficaria/missing_imputation.py
@@ -1,4 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.exceptions import NotFittedError
+from sklearn.utils.validation import check_is_fitted
 
 from .utils import *
 
@@ -213,10 +215,14 @@ class KIImputer(BaseEstimator, TransformerMixin):
     and the imputation is refined using a model-based iterative approach.
     """
 
-    def __init__(self, random_state=None):
+    def __init__(self, random_state: Optional[int] = None, max_iter: int = 30):
         if random_state is not None and not isinstance(random_state, int):
-            raise TypeError('Invalid random_state: Expected an integer or None.')
+            raise TypeError('Invalid random_state: Expected an integer or None')
+        if not isinstance(max_iter, int) or max_iter <= 1:
+            raise TypeError('Invalid max_iter: Expected a positive integer')
+
         self.random_state = random_state
+        self.max_iter = max_iter
         pass
 
     def fit(self, X, y=None):
@@ -227,7 +233,13 @@ class KIImputer(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         X = check_input_dataset(X)
-        X_imputed = impute_KI(X, self.X_train_, np_rng=self.np_rng_)
+        check_is_fitted(self, attributes=["X_train_", "np_rng_"])
+        if not X.columns.equals(self.X_train_.columns):
+            raise ValueError(
+                f"Invalid input: Input dataset columns do not match columns seen during fit"
+            )
+
+        X_imputed = impute_KI(X, self.X_train_, np_rng=self.np_rng_, max_iter=self.max_iter)
         return X_imputed
 
 
@@ -241,19 +253,23 @@ class FCMKIterativeImputer(BaseEstimator, TransformerMixin):
    similarity search enhances accuracy compared to standard KNN imputation.
     """
 
-    def __init__(self, random_state=None, max_clusters=10, m=2):
+    def __init__(self, random_state: Optional[int] = None, max_clusters: int = 10, m: float = 2, max_iter: int = 30):
         if random_state is not None and not isinstance(random_state, int):
-            raise TypeError('Invalid random_state: Expected an integer or None.')
+            raise TypeError('Invalid random_state: Expected an integer or None')
 
         if not isinstance(max_clusters, int) or max_clusters <= 1:
-            raise TypeError('Invalid max_clusters: Expected an integer greater than 1.')
+            raise TypeError('Invalid max_clusters: Expected an integer greater than 1')
 
         if not isinstance(m, (int, float)) or m <= 1:
-            raise TypeError('Invalid m value: Expected a numeric value greater than 1.')
+            raise TypeError('Invalid m value: Expected a numeric value greater than 1')
+
+        if max_iter is not None and not isinstance(max_iter, int) or max_iter <= 1:
+            raise TypeError('Invalid max_iter: Expected a positive integer greater than 1')
 
         self.random_state = random_state
         self.max_clusters = max_clusters
         self.m = m
+        self.max_iter = max_iter
         pass
 
     def fit(self, X, y=None):
@@ -265,8 +281,7 @@ class FCMKIterativeImputer(BaseEstimator, TransformerMixin):
         X_filled = pd.DataFrame(data=X_filled, columns=X.columns, index=X.index)
 
         self.optimal_c_ = find_optimal_clusters_fuzzy(X_filled, min_clusters=1, max_clusters=self.max_clusters,
-                                                      m=self.m,
-                                                      random_state=self.random_state)
+                                                      m=self.m, random_state=self.random_state)
         self.np_rng_ = np.random.RandomState(self.random_state)
         np.random.seed(self.random_state)
 
@@ -281,7 +296,13 @@ class FCMKIterativeImputer(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         X = check_input_dataset(X, require_numeric=True)
+        check_is_fitted(self, attributes=["X_train_", "imputer_", "centers_", "u_", "optimal_c_", "np_rng_"])
+
+        if not X.columns.equals(self.X_train_.columns):
+            raise ValueError(
+                f"Invalid input: Input dataset columns do not match columns seen during fit"
+            )
 
         X_imputed = impute_FCKI(X, self.X_train_, self.centers_, self.u_, self.optimal_c_, self.imputer_, self.m,
-                                self.np_rng_, self.random_state)
+                                self.np_rng_, self.random_state, max_iter=self.max_iter)
         return X_imputed
